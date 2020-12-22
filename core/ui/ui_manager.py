c35f214130b27653a042616d366bacdc56f9b3ab
ivan-moreno <ivan@bgeo.es>
2020-12-22 10:57:15 +0100

    UI Added

--- core/ui/ui_manager.py
+++ core/ui/ui_manager.py
@@ -148,6 +148,10 @@ FORM_CLASS = get_ui_class('dlg_boton2.ui', 'my_plugin')
 class DlgBoton2(GwDialog, FORM_CLASS):
     pass
 
+FORM_CLASS = get_ui_class('dlg_boton3.ui', 'my_plugin')
+class DlgBoton3(GwDialog, FORM_CLASS):
+    pass
+
 FORM_CLASS = get_ui_class('mincut_manager.ui', 'my_plugin')
 class MincutManager(GwDialog, FORM_CLASS):
     pass
