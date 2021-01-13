2b710cc9e7d5e6b6e49d19ba1cc1bd164c2fa5a0
Néstor Ibáñez <nestor@bgeo.es>
2021-01-13 11:46:17 +0100

    Get folder path ussing environment variables or plugin root folder

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-import os, sys, glob, importlib
+import configparser, os, sys, glob, importlib
 
 # Pointer to the module object instance itself
 this = sys.modules[__name__]
@@ -23,22 +23,40 @@ def init_plugin():
         return
 
     # Define imports to Giswater modules
-    this.tools_qgis = importlib.import_module('.tools_qgis', package=f'{giswater_folder}.lib')
-    this.tools_qt = importlib.import_module('.tools_qt', package=f'{giswater_folder}.lib')
-    this.tools_gw = importlib.import_module('.tools_gw', package=f'{giswater_folder}.core.utils')
+    this.tools_qgis = importlib.import_module('.tools_qgis', package=f'{this.giswater_folder}.lib')
+    this.tools_qt = importlib.import_module('.tools_qt', package=f'{this.giswater_folder}.lib')
+    this.tools_gw = importlib.import_module('.tools_gw', package=f'{this.giswater_folder}.core.utils')
 
 
-def get_giswater_folder(filename_to_find='tools_qgis.py'):
+def get_giswater_folder(filename_to_find='metadata.txt'):
     """ Find and return Giswater plugin folder name """
 
-    # Get QGIS plugin root folder
-    qgis_plugin_root_folder = os.path.dirname(os.path.dirname(__file__))
+    # Get QGIS plugin root folder from environment variables
+    qgis_plugin_root_folder = None
+    try:
+        if sys.platform == "win32":
+            qgis_plugin_root_folder = os.environ['QGIS_PLUGINPATH']
+        elif sys.platform == "linux":
+            qgis_plugin_root_folder = os.environ['QGIS_PLUGINPATH']
+        elif sys.platform == "darwin":
+            qgis_plugin_root_folder = os.environ['QGIS_PLUGINPATH']
+    except KeyError:
+        # Key not found
+        pass
+
+    # Get QGIS plugin root folder from qgis plugin path
+    if qgis_plugin_root_folder is None:
+        qgis_plugin_root_folder = os.path.dirname(os.path.dirname(__file__))
 
     # Find @filename recursively inside this folder
     for filename in glob.glob(f"{qgis_plugin_root_folder}/**/{filename_to_find}", recursive=True):
-        giswater_folder_name = os.path.basename(os.path.dirname(os.path.dirname(filename)))
-        print(f"giswater_folder_name: {giswater_folder_name}")
-        return giswater_folder_name
+        parser = configparser.ConfigParser()
+        parser.read(filename)
+        if not parser.has_section('general'): continue
+        if not parser.has_option('general', 'name'): continue
+        if parser['general']['name'] == 'giswater':
+            giswater_folder_name = os.path.basename(os.path.dirname(filename))
+            return giswater_folder_name
 
     return None
 
@@ -47,3 +65,5 @@ def init_global():
 
     print(f"init_global: {this.giswater_folder}")
     this.global_vars = importlib.import_module('.global_vars', package=f'{this.giswater_folder}')
+
+
